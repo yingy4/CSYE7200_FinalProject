220e54d51adcc5fa376bf8b1f04825b05a0a7c59
xin wen <wen.xin1@husky.neu.edu>
2017-12-06 18:31:16 -0500

    Update models in core

--- core/src/main/scala/folder/ImportFolder.scala
+++ core/src/main/scala/folder/ImportFolder.scala
@@ -13,10 +13,11 @@ object ImportFolder extends App{
 
   val dirPath = "test_image"
 
-  val test_images = spark.sparkContext.wholeTextFiles(dirPath, 5).map (x=>x._1)
-    .filter(x => x.endsWith(".jpg")|| x.endsWith(".png")|| x.endsWith("jpeg"))
-    .map(_.substring(5))
-
+  def readFromFolder() {
+    val test_images = spark.sparkContext.wholeTextFiles(dirPath, 5).map(x => x._1)
+      .filter(x => x.endsWith(".jpg") || x.endsWith(".png") || x.endsWith("jpeg"))
+      .map(_.substring(5))
+  }
   test_images.foreach(ImageFaceDetector.markFace(_))
   test_images.foreach(TensorLabel.detectBreed(_))
 
