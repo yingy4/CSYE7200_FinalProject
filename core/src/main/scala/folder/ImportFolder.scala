605f1f6000e68663d4333373f61a758ed64e1028
xin wen <wen.xin1@husky.neu.edu>
2017-12-06 18:34:45 -0500

    Modify importFolder

--- core/src/main/scala/folder/ImportFolder.scala
+++ core/src/main/scala/folder/ImportFolder.scala
@@ -2,6 +2,7 @@ package folder
 
 import dogIdentify.TensorLabel
 import faceDetect.ImageFaceDetector
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object ImportFolder extends App{
@@ -13,11 +14,14 @@ object ImportFolder extends App{
 
   val dirPath = "test_image"
 
-  def readFromFolder() {
+  def readFromFolder(dirPath:String):RDD[String]= {
     val test_images = spark.sparkContext.wholeTextFiles(dirPath, 5).map(x => x._1)
       .filter(x => x.endsWith(".jpg") || x.endsWith(".png") || x.endsWith("jpeg"))
       .map(_.substring(5))
+    test_images
   }
+
+  val test_images = readFromFolder(dirPath)
   test_images.foreach(ImageFaceDetector.markFace(_))
   test_images.foreach(TensorLabel.detectBreed(_))
 
